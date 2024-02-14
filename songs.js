01f2cccc23483ed5152e627cadaa08cb450167cb
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-02-14 12:00:31 -0400

    hice el track list

--- songs.js
+++ songs.js
@@ -1,1 +1,22 @@
+const respuesta = axios.get("https://leonardoapi.onrender.com/songs")
+         .then((res)=>{
+            res.data.songs.map( (song) => {
+              
+                let div = document.createElement('div')
+                div.classList.add('songs')
 
+                div.innerHTML = `
+                <img src="${song.path.front}" alt="">
+                <div>
+                <span>${song.title}</span>
+                <p>${song.author}</p>
+                </div>`
+
+                div.addEventListener('click', () => {
+
+                    document.getElementById('current-song-audio').setAttribute('src', song.path.audio)
+                })
+
+                document.getElementById('track-list').appendChild(div)
+            })
+         })
